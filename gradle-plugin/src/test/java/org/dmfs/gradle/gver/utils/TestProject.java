efe33e5fbb002dd722140793d36b57af6f727695
Marten Gajda <marten@dmfs.org>
2023-09-03 21:59:09 +0200

    Rewrite tests with new Quality improvement, closes #85 (#86)
    This uses the improved `procedureThatAffects` quality to get rid of the
    imperative exception handling.
    
    In addition this commit replaces the TagTaskTest with a simpler one that
    uses the Confidence framework.

--- gradle-plugin/src/test/java/org/dmfs/gradle/gver/utils/TestProject.java
+++ gradle-plugin/src/test/java/org/dmfs/gradle/gver/utils/TestProject.java
@@ -2,7 +2,9 @@ package org.dmfs.gradle.gver.utils;
 
 import org.dmfs.gver.dsl.GitVersionConfig;
 import org.dmfs.gver.dsl.Strategy;
+import org.dmfs.gver.git.Suffixes;
 import org.dmfs.jems2.Fragile;
+import org.dmfs.jems2.FragileFunction;
 import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.saynotobugs.confidence.junit5.engine.assertion.WithResource;
@@ -10,24 +12,43 @@ import org.saynotobugs.confidence.junit5.engine.assertion.WithResource;
 import java.io.File;
 
 
-public final class TestProject implements Fragile<WithResource.Resource<Project>, Exception>
+public final class TestProject implements Fragile<WithResource.Resource<Project>, Exception>,
+    FragileFunction<File, WithResource.Resource<Project>, Exception>
 {
     private final File mProjectDir;
     private final Strategy mStrategy;
+    private final Suffixes mSuffixes;
 
     public TestProject(File projectDir, Strategy strategy)
     {
+        this(projectDir, strategy, new Suffixes());
+    }
+
+    public TestProject(File projectDir, Strategy strategy, Suffixes suffixes)
+    {
         mProjectDir = projectDir;
         mStrategy = strategy;
+        mSuffixes = suffixes;
     }
 
 
     @Override
     public WithResource.Resource<Project> value() throws Exception
     {
-        Project project = ProjectBuilder.builder().withProjectDir(mProjectDir).build();
+        return value(mProjectDir);
+    }
+
+    @Override
+    public WithResource.Resource<Project> value(File file) throws Exception
+    {
+        Project project = ProjectBuilder.builder()
+            .withProjectDir(mProjectDir)
+            .withGradleUserHomeDir(file)
+            .build();
         project.getPluginManager().apply("org.dmfs.gver");
         ((GitVersionConfig) project.getExtensions().getByName("gver")).mChangeTypeStrategy = mStrategy;
+        ((GitVersionConfig) project.getExtensions().getByName("gver")).mSuffixes = mSuffixes;
+
 
         return new WithResource.Resource<Project>()
         {
@@ -46,5 +67,4 @@ public final class TestProject implements Fragile<WithResource.Resource<Project>
 
         };
     }
-
 }
