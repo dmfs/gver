e1dc9baf844b706c27390b4b5b1248f878a84151
Marten Gajda <marten@dmfs.org>
2023-09-12 16:58:52 +0200

    Refuse tagging when the Git working tree has uncommitted changes, closes #4  (#87)

--- gradle-plugin/src/test/java/org/dmfs/gradle/gver/tasks/TagTaskTest.java
+++ gradle-plugin/src/test/java/org/dmfs/gradle/gver/tasks/TagTaskTest.java
@@ -11,6 +11,7 @@ import org.dmfs.jems2.iterable.Mapped;
 import org.dmfs.jems2.optional.Present;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Ref;
+import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.saynotobugs.confidence.description.Text;
 import org.saynotobugs.confidence.junit5.engine.Assertion;
@@ -23,8 +24,7 @@ import java.io.File;
 import static org.dmfs.gver.git.ChangeType.*;
 import static org.dmfs.jems2.confidence.Jems2.procedureThatAffects;
 import static org.eclipse.jgit.lib.Constants.R_TAGS;
-import static org.saynotobugs.confidence.junit5.engine.ConfidenceEngine.assertionThat;
-import static org.saynotobugs.confidence.junit5.engine.ConfidenceEngine.withResources;
+import static org.saynotobugs.confidence.junit5.engine.ConfidenceEngine.*;
 import static org.saynotobugs.confidence.quality.Core.*;
 
 
@@ -54,4 +54,49 @@ class TagTaskTest
                         () -> repo,
                         soIt(has(repository -> new Mapped<>(Ref::getName, new Git(repository).tagList().call()),
                             iteratesInAnyOrder(R_TAGS + "0.0.1", R_TAGS + "0.0.2-alpha.1-SNAPSHOT"))))))));
+
+
+    Assertion gitTagRelease_fails_on_dirty_repo =
+        withResources("with temporary directory and git repository",
+            new TempDir(),
+            new Repository(getClass().getClassLoader().getResource("0.0.2-alpha.1.bundle"), "main"),
+            // we use a separate gradle home dir to make this test pass
+            (tempDir, repo) -> withResources("temporary user home dir and project",
+                new TempDir(),
+                (FragileFunction<File, WithResource.Resource<Project>, Exception>) new TestProject(tempDir,
+                    new Strategy(
+                        MAJOR.when(new CommitMessage(new Contains("#major"))),
+                        MINOR.when(new CommitMessage(new Contains("#minor"))),
+                        PATCH.when(new CommitMessage(new Contains("#patch"))),
+                        UNKNOWN.when(((repository1, commit, branches) -> true)))),
+
+                (userHome, project) ->
+                    withResource(() -> {
+                            new File(tempDir, "newfile").createNewFile();
+                            new Git(repo).add().addFilepattern("newfile").call();
+                            return new WithResource.Resource<org.eclipse.jgit.lib.Repository>()
+                            {
+                                @Override
+                                public void close()
+                                {
+                                    // nothing to do here, this will be closed by the outer resource
+                                }
+
+                                @Override
+                                public org.eclipse.jgit.lib.Repository value()
+                                {
+                                    return repo;
+                                }
+                            };
+                        },
+                        innerRepo ->
+                            assertionThat(repository -> ((TagTask) project.getTasks().getByName("gitTag")).perform(),
+                                is(procedureThatAffects(
+                                    new Text("alters repository"),
+                                    () -> repo,
+                                    soIt(has(repository -> new Mapped<>(Ref::getName, new Git(repository).tagList().call()),
+                                        // same tags as before
+                                        iteratesInAnyOrder(R_TAGS + "0.0.1"))),
+                                    when(throwing(GradleException.class))))))));
+
 }
